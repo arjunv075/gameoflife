3f71f6032d38a581cf72383122368e593182c610
arjunv075 <arjunv075@gmail.com>
2023-11-08 15:49:39 +0530

    GREEN: add count_neighbours()

--- game_of_life.py
+++ game_of_life.py
@@ -12,3 +12,15 @@ def create_grid(rows, cols):
 def print_grid(grid):
     for row in grid:
         print(" ".join(["." if cell else " " for cell in row]))
+
+def count_neighbours(grid, x, y):
+    rows, cols = len(grid), len(grid[0])
+    neighbours = [(x-1, y-1), (x-1, y), (x-1, y+1),
+                (x, y-1),               (x, y+1),
+                (x+1, y-1), (x+1, y), (x+1, y+1)]
+    
+    count = 0
+    for x, y in neighbours:
+        if 0 <= x < rows and 0 <= y < cols and grid[x][y] == 1:
+            count += 1
+    return count
