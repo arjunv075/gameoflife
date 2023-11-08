67767b33e86e9a42e04fcfceaf1bff4bb061edc9
arjunv075 <arjunv075@gmail.com>
2023-11-08 15:46:06 +0530

    GREEN: add print_grid()

--- game_of_life.py
+++ game_of_life.py
@@ -8,3 +8,7 @@ def create_grid(rows, cols):
         row = [random.randint(0, 1) for _ in range(cols)]
         grid.append(row)
     return grid
+
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(["." if cell else " " for cell in row]))
