7b831ca3f3d593c470c9091ee55643035d14b690
arjunv075 <arjunv075@gmail.com>
2023-11-08 16:04:01 +0530

    GREEN: add next_generation()

--- game_of_life.py
+++ game_of_life.py
@@ -24,3 +24,21 @@ def count_neighbours(grid, x, y):
         if 0 <= x < rows and 0 <= y < cols and grid[x][y] == 1:
             count += 1
     return count
+
+def next_generation(current_grid):
+    rows, cols = len(current_grid), len(current_grid[0])
+    new_grid = copy.deepcopy(current_grid)
+
+    for x in range(rows):
+        for y in range(cols):
+            cell = current_grid[x][y]
+            neighbours = count_neighbours(current_grid, x, y)
+
+            if cell == 1:
+                if neighbours < 2 or neighbours > 3:
+                    new_grid[x][y] = 0
+            else:
+                if neighbours == 3:
+                    new_grid[x][y] = 1
+
+    return new_grid
