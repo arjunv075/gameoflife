c469a4e0e2fd85aaf4946d12d8e9f4535a5ffd78
arjunv075 <arjunv075@gmail.com>
2023-11-09 12:59:20 +0530

    rename create_grid() to create_random_grid()

--- game_of_life.py
+++ game_of_life.py
@@ -2,7 +2,7 @@ import random
 import time
 import copy
 
-def create_grid(rows, cols):
+def create_random_grid(rows, cols):
     grid = []
     for _ in range(rows):
         row = [random.randint(0, 1) for _ in range(cols)]
